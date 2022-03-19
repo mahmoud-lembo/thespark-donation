19abdfaa7ad3c160378ebb752e05e0898d993945
Lembo <mahmoud.m.abdelmalek@gmail.com>
2022-03-20 01:44:26 +0200

    Updates

--- donate/backend/donate.php
+++ donate/backend/donate.php
@@ -5,28 +5,26 @@
     $payment = $_POST["payment"];
     $email = $_POST["email"];
     $amount = $_POST["amount"];
+    $subject = 'Donation Invoice';
 
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $name . "\r\n"; // Sender's E-mail
+    $headers .= "From: " . $name . "<".$email.">\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
     $message ='<table style="width:100%">
-Charity Donation
         <tr>
-            <td>'.$name.'  '.$email.'</td>
+            <td>Name'.$name.' </br>  Email:'.$email.'</td>
         </tr>
         <tr><td>Payment Method: '.$payment.'</td></tr>
         <tr><td>Project: '.$type.'</td></tr>
-        <tr><td>Amount: '.$amount.'</td></tr>     
+        <tr><td>Amount: '.$amount.'$</td></tr>     
     </table>';
 
-    if (@mail($to, $email, $message, $headers))
+    if (@mail($to, $subject, $message, $headers))
     {
-        echo 'The message has been sent.';
-        if($_POST["type"] == "paypal"){
+        echo 'Redirecting.';
         echo '<meta http-equiv="refresh" content="0;url=https://www.paypal.com/cgi-bin/webscr?business=mahmoud.m.abdelmalek@gmail.com&cmd=_xclick&currency_code=USD&amount='.$_POST["amount"].'&item_name=Donate">';
-        }
     }else{
         echo 'failed';
     }
